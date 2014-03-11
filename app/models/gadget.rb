22983505424ffdcc5e291ff4f0d6ea0b1f14ff14
Claus Skou Nielsen <kalusn@gmail.com>
2014-03-11 16:46:59 +0100

    Gadget tests passing

--- app/models/gadget.rb
+++ app/models/gadget.rb
@@ -1,2 +1,5 @@
 class Gadget < ActiveRecord::Base
+  belongs_to :user
+
+  validates_presence_of :name, :description, :user_id
 end
--- spec/models/gadget_spec.rb
+++ spec/models/gadget_spec.rb
@@ -1,21 +1,17 @@
 require 'spec_helper'
 
 describe Gadget do
-  before(:all) do
-    @default_gadget = build(:gadget)
-    @user = build(:user)
-  end
   describe "validations" do
-    before(:all) do
-      @default_gadget.user = @user
-    end
-
     it "should be valid when filled out correctly" do
+      @gadget = build(:gadget)
+      @user = build(:user)
       @gadget.should be_valid
     end
 
     describe "name" do
       it "should require name field" do
+        @gadget = build(:gadget)
+        @user = build(:user)
         @gadget.name = ""
         @gadget.should_not be_valid
       end
@@ -23,6 +19,8 @@ describe Gadget do
     
     describe "description" do
       it "should require a description" do
+        @gadget = build(:gadget)
+        @user = build(:user)
         @gadget.description = ""
         @gadget.should_not be_valid
       end
@@ -30,7 +28,9 @@ describe Gadget do
 
     describe "user" do
       it "should require an owner" do
-        @gadget.user = ""
+        @gadget = build(:gadget)
+        @user = build(:user)
+        @gadget.user_id = ""
         @gadget.should_not be_valid
       end
     end
@@ -39,9 +39,11 @@ describe Gadget do
   describe "relations" do
     describe "user" do
       it "should be able to belong to a user" do
-        @user << @default_gadget
-        @default_gadget.save
-        @default_gadget.user.should == @user
+        @gadget = build(:gadget)
+        @user = build(:user)
+        @gadget.user = @user
+        @gadget.save
+        @gadget.user.should == @user
       end
     end
 
