745e00db56b4cca27a75a8c8a53e8284bdcf92c0
Claus Skou Nielsen <kalusn@gmail.com>
2014-03-11 15:37:11 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gadgenizer::Application.config.secret_key_base = '203e12255c4c0f5798bae34c62b05db9d56e26c376b0dd344db12318580e3d3836679c1a0a07c237694ee38e77b3601a6ff38e241709db41bdefaa625fbbd964'
